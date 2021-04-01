a9b56aadfee0223a1d6ea973656c713d09a6c7f9
hellfreezes <hellfreezes@yandex.ru>
2021-04-01 21:20:54 +0700

    jwt

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserSchema } from "./entities/user.model";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
